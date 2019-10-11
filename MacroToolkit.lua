33ffa886e60c95bbb061cf6928b8d1f1cae4b1b5
Tomás Migone <tomasmigone@gmail.com>
2019-10-10 22:08:31 -0300

    Added weakauras and macros

---
+++ MacroToolkit.lua
@@ -0,0 +1,247 @@
+
+MacroToolkitDB = {
+	["char"] = {
+		["Galaen - Lightbringer"] = {
+			["backups"] = {
+				{
+					["m"] = {
+						{
+							["icon"] = 1373912,
+							["index"] = 121,
+							["name"] = " ",
+							["body"] = "#showtooltip Shadowstrike\n/cast Pick Pocket\n/cast [bonusbar:1] Shadowstrike;Backstab\n",
+						}, -- [1]
+						{
+							["icon"] = 236279,
+							["index"] = 122,
+							["name"] = " ",
+							["body"] = "#showtooltip Shadow Dance\n/cast [stance:0] Shadow Dance\n/cast Shadowstrike",
+						}, -- [2]
+						{
+							["icon"] = 132331,
+							["index"] = 123,
+							["name"] = " ",
+							["body"] = "#showtooltip Vanish\n/cast [nocombat] Stealth\n/cast [combat] Vanish\n",
+						}, -- [3]
+						{
+							["icon"] = 132320,
+							["index"] = 124,
+							["name"] = " ",
+							["body"] = "#showtooltip Stealth\n/dismount\n/stopcasting\n/cast [nocombat] !Stealth\n/cast [combat] Vanish\n",
+						}, -- [4]
+						{
+							["icon"] = 132303,
+							["index"] = 125,
+							["name"] = " ",
+							["body"] = "#showtooltip Shadowstep\n/cast Shadowstep\n/cast Pick Pocket\n",
+						}, -- [5]
+						{
+							["icon"] = 132298,
+							["index"] = 126,
+							["name"] = " ",
+							["body"] = "#showtooltip Kidney Shot\n/cast [stance:0] Kidney Shot\n/cast [stance:1/2/3] Cheap Shot\n",
+						}, -- [6]
+						{
+							["icon"] = 134723,
+							["index"] = 127,
+							["name"] = " ",
+							["body"] = "#showtooltip Potion of Prolonged Power\n/use Potion of Prolonged Power\n/cast Shadow Blades\n/cast Stealth\n",
+						}, -- [7]
+						{
+							["icon"] = 134780,
+							["index"] = 128,
+							["name"] = "  ",
+							["body"] = "#showtooltip Crimson Vial\n/cast Healthstone\n/cast  Crimson Vial",
+						}, -- [8]
+					},
+					["d"] = "Dec 25 14:38:52",
+					["n"] = "Galen",
+				}, -- [1]
+			},
+			["lastbackup"] = "Dec 25 14:38:52",
+		},
+		["Gäla - Ragnaros"] = {
+			["backups"] = {
+			},
+		},
+		["Gälean - Ragnaros"] = {
+			["backups"] = {
+			},
+		},
+		["Gälaen - Ragnaros"] = {
+			["backups"] = {
+			},
+		},
+	},
+	["global"] = {
+		["backups"] = {
+			{
+				["m"] = {
+					{
+						["name"] = " ",
+						["icon"] = 236279,
+						["index"] = 1,
+						["body"] = "#showtooltip Shadow Dance\n/cast [stance:0] Shadow Dance\n/cast Shadowstrike\n",
+					}, -- [1]
+					{
+						["name"] = " ",
+						["icon"] = 132331,
+						["index"] = 2,
+						["body"] = "#showtooltip Vanish\n/cast [nocombat] Stealth\n/cast [combat] Vanish\n",
+					}, -- [2]
+					{
+						["name"] = " ",
+						["icon"] = 132320,
+						["index"] = 3,
+						["body"] = "#showtooltip Stealth\n/dismount\n/stopcasting\n/cast [nocombat] !Stealth\n/cast [combat] Vanish\n",
+					}, -- [3]
+					{
+						["name"] = " ",
+						["icon"] = 132303,
+						["index"] = 4,
+						["body"] = "#showtooltip Shadowstep\n/cast Shadowstep\n/cast Pick Pocket\n",
+					}, -- [4]
+					{
+						["name"] = " ",
+						["icon"] = 132298,
+						["index"] = 5,
+						["body"] = "#showtooltip Kidney Shot\n/cast [stance:0] Kidney Shot\n/cast [stance:1/2/3] Cheap Shot\n",
+					}, -- [5]
+					{
+						["name"] = " ",
+						["icon"] = 1373912,
+						["index"] = 6,
+						["body"] = "#showtooltip Shadowstrike\n/cast Pick Pocket\n/cast [bonusbar:1] Shadowstrike;Backstab\n",
+					}, -- [6]
+					{
+						["name"] = " ",
+						["icon"] = 376022,
+						["index"] = 7,
+						["body"] = "#showtooltip Shadow Blades\n/use Battle Potion of Agility\n/cast Shadow Blades\n/cast [nocombat] Stealth\n/cast [combat] Shadowmeld\n",
+					}, -- [7]
+					{
+						["name"] = " ",
+						["icon"] = 1354171,
+						["index"] = 8,
+						["body"] = "",
+					}, -- [8]
+					{
+						["name"] = " ",
+						["icon"] = 132294,
+						["index"] = 9,
+						["body"] = "#showtooltip Feint\n/cast Feint\n/cast Evasion\n",
+					}, -- [9]
+					{
+						["name"] = " ",
+						["icon"] = 136175,
+						["index"] = 10,
+						["body"] = "#showtooltip\n/cast Blind\n/stopattack\n",
+					}, -- [10]
+					{
+						["name"] = "  ",
+						["icon"] = 134780,
+						["index"] = 11,
+						["body"] = "#showtooltip Crimson Vial\n/cast Healthstone\n/cast  Crimson Vial\n",
+					}, -- [11]
+				},
+				["d"] = "Dec 25 20:33:18",
+				["n"] = "macros",
+			}, -- [1]
+			{
+				["m"] = {
+					{
+						["name"] = " ",
+						["icon"] = 236279,
+						["index"] = 1,
+						["body"] = "#showtooltip Shadow Dance\n/cast [stance:0] Shadow Dance\n/cast Shadowstrike\n",
+					}, -- [1]
+					{
+						["name"] = " ",
+						["icon"] = 132331,
+						["index"] = 2,
+						["body"] = "#showtooltip Vanish\n/cast [nocombat] Stealth\n/cast [combat] Vanish\n",
+					}, -- [2]
+					{
+						["name"] = " ",
+						["icon"] = 132320,
+						["index"] = 3,
+						["body"] = "#showtooltip Stealth\n/dismount\n/stopcasting\n/cast [nocombat] !Stealth\n/cast [combat] Vanish\n",
+					}, -- [3]
+					{
+						["name"] = " ",
+						["icon"] = 132303,
+						["index"] = 4,
+						["body"] = "#showtooltip Shadowstep\n/cast Shadowstep\n/cast Pick Pocket\n",
+					}, -- [4]
+					{
+						["name"] = " ",
+						["icon"] = 132298,
+						["index"] = 5,
+						["body"] = "#showtooltip Kidney Shot\n/cast [stance:0] Kidney Shot\n/cast [stance:1/2/3] Cheap Shot\n",
+					}, -- [5]
+					{
+						["name"] = " ",
+						["icon"] = 1373912,
+						["index"] = 6,
+						["body"] = "#showtooltip Shadowstrike\n/cast Pick Pocket\n/cast [bonusbar:1] Shadowstrike;Backstab\n",
+					}, -- [6]
+					{
+						["name"] = " ",
+						["icon"] = 376022,
+						["index"] = 7,
+						["body"] = "#showtooltip Shadow Blades\n/use Battle Potion of Agility\n/cast Shadow Blades\n/cast [nocombat] Stealth\n/cast [combat] Shadowmeld\n",
+					}, -- [7]
+					{
+						["name"] = " ",
+						["icon"] = 1354171,
+						["index"] = 8,
+						["body"] = "",
+					}, -- [8]
+					{
+						["name"] = " ",
+						["icon"] = 132294,
+						["index"] = 9,
+						["body"] = "#showtooltip Feint\n/cast Feint\n/cast Evasion\n",
+					}, -- [9]
+					{
+						["name"] = " ",
+						["icon"] = 136175,
+						["index"] = 10,
+						["body"] = "#showtooltip\n/cast Blind\n/stopattack\n",
+					}, -- [10]
+					{
+						["name"] = "  ",
+						["icon"] = 134780,
+						["index"] = 11,
+						["body"] = "#showtooltip Crimson Vial\n/cast Healthstone\n/cast  Crimson Vial\n",
+					}, -- [11]
+				},
+				["d"] = "Dec 25 20:33:32",
+				["n"] = "TOMIMACRO",
+			}, -- [2]
+		},
+		["extended"] = {
+			["1"] = {
+				["name"] = " ",
+				["icon"] = "413588",
+				["body"] = "#showtooltip\n/use [mod:alt]Traveler's Tundra Mammoth;[swimming]Azure Water Strider\n/run if SecureCmdOptionParse\"[nomod]\"then C_MountJournal.SummonByID(0)end",
+			},
+		},
+		["lastbackup"] = "Dec 25 20:33:32",
+		["ebackups"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Jargarr - Ragnaros"] = "profile",
+		["Galaen - Lightbringer"] = "profile",
+		["Gälaen - Ragnaros"] = "profile",
+		["Gälean - Ragnaros"] = "profile",
+		["Gäla - Ragnaros"] = "profile",
+	},
+	["profiles"] = {
+		["profile"] = {
+			["x"] = 699.311157226563,
+			["y"] = 535.520080566406,
+		},
+	},
+}
